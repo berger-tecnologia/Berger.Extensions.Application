6bf84f12576e1b67ad0e7e15848c5face68ee883
Rafael Cronemberger <rafael@berger.com.br>
2023-11-27 09:55:17 -0300

    Package updates

---
+++ src/Berger.Extensions.Application/Services/BaseApplicationGetTemp.cs
@@ -0,0 +1,23 @@
+﻿using Berger.Extensions.Abstractions;
+
+namespace Berger.Extensions.Application
+{
+    public class BaseApplicationGetTemp<T> : IBaseApplicationGet<T> where T : BaseEntity
+    {
+        private readonly List<T> _items;
+
+        public BaseApplicationGetTemp(List<T> items)
+        {
+            _items = items;
+        }
+        public List<T> Get()
+        {
+            return _items;
+        }
+
+        public T GetById(Guid id)
+        {
+            return _items.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
